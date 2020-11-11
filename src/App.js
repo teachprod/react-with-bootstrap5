258c1a5f0d363f908f79177dfa0d2191c017ccdc
Dang Chi <chid@jobready.com.au>
2020-11-11 21:48:18 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+    </div>
+  );
+}
+
+export default App;
